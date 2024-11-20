89ae6b1cd1e888486141667906a6b6549bc68e98
iychoi <iychoi@direcionsoft.com>
2024-11-20 15:58:48 +0900

    day1 프로젝트 추가

---
+++ dailychallenge_day1/dailychallenge_day1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dailychallenge_day1
+//
+//  Created by directionsoft on 11/20/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
