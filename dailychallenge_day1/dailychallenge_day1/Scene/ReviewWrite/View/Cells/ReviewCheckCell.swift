66112261328ebe39f6cbe7f932942f2099765a2f
iychoi <iychoi@direcionsoft.com>
2024-11-21 14:23:02 +0900

    Cells 디자인 작업 중

--- dailychallenge_day1/dailychallenge_day1/Scene/ReviewWrite/View/Cells/ReviewCheckCell.swift
+++ dailychallenge_day1/dailychallenge_day1/Scene/ReviewWrite/View/Cells/ReviewCheckCell.swift
@@ -5,6 +5,15 @@
 //  Created by directionsoft on 11/20/24.
 //
 
+let REVIEW_CHECK_OPTIONS: [String] = [
+    "친절했어요",
+    "정직하게 검차했어요",
+    "일처리가 신속 정확해요",
+    "약속을 잘 지켰어요",
+    "자세한 설명을 해주셨어요",
+    "기타"
+]
+
 import UIKit
 
 class ReviewCheckCell: UITableViewCell {
--- dailychallenge_day1/dailychallenge_day1/Scene/ReviewWrite/View/Cells/ReviewWriteCell.swift
+++ dailychallenge_day1/dailychallenge_day1/Scene/ReviewWrite/View/Cells/ReviewWriteCell.swift
@@ -9,15 +9,9 @@ import UIKit
 
 class ReviewWriteCell: UITableViewCell {
 
+    // textField 다른 곳 터치 시 키보드 닫기
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
-    
 }
